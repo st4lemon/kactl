bfc06c3e1b8b64aa289f6ab66cc36cfb55676730
Om Mahesh <2om.mahesh@gmail.com>
2025-09-04 14:39:31 -0700

    added CountPrimes.h to kactl.pdf

--- content/number-theory/CountPrimes.h
+++ content/number-theory/CountPrimes.h
@@ -2,8 +2,8 @@
  * Author: Om Mahesh
  * Date: 2025-09-04
  * Source: https://codeforces.com/blog/entry/91632
- * Description: Counts primes up till n in O(n^{3/4})
- * Time: n=10^11 $\approx$ 1 seconds, n=10^12 $\approx$ 5 seconds
+ * Description: Counts primes up till n in $O(n^{3/4})$
+ * Time: $n=10^{11}$ $\approx$ 1 seconds, $n=10^{12}$ $\approx$ 5 seconds
  * Status: Tested
  */
 
@@ -11,10 +11,8 @@ ll count_primes(ll n) {
     vector<ll> v;
     for (ll i = 1; i <= n; i = n / (n / i) + 1) v.push_back(n / i);
     reverse(all(v));
-
     int sq = sqrt(n);
     auto geti = [&](ll x) { return x <= sq ? x - 1 : sz(v) - n / x; };
-
     vector<ll> dp = v;
     for (ll p = 2, a = 0; p <= sq; p++) {
         if (dp[geti(p)] == dp[geti(p - 1)]) continue;
@@ -24,6 +22,5 @@ ll count_primes(ll n) {
             dp[i] -= dp[geti(v[i] * inv + 1e-9)] - a;
         }
     }
-
     return dp[geti(n)] - 1;
 }
